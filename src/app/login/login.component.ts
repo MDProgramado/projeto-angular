054fb6eb1bd78a82564f2dae89d20cdafb33a039
Maicon Douglas <mdprogamacao@outlook.com>
2025-04-24 16:56:42 -0300

    dando validação no formulario e deixando ele dimanico

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,36 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Usuario } from '../../../models/usuario.model';
 
 @Component({
+  standalone: true,
   selector: 'app-login',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
   imagemLogo: string = "assets/img/ford.png";
   imagemFundo: string = "assets/img/mustang.png";
   titulo: string = "Boas-vindas";
-  login: string = "email";
-  senha: string = "senha";
+  usuario: Usuario = {
+    id: '',
+    nome: '',
+    email: '',
+    senha: ''
+  };
+
+  formulario = new FormGroup({
+    nome: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required,Validators.email]),
+    senha: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(20), 
+      Validators.pattern('^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]+$')])
+  });
+
+
+  
+
   butao: string = "Entrar";
 }
